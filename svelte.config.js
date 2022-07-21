94e8804b73ab9431dc49d39574e827c61b4197b0
Fernando Nogueira <abert036@uottawa.ca>
2022-07-21 09:42:20 -0400

    s

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
 import sveltePreprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
+
+// Check if environment variables contains GITHUB_ACTION
+let pathBase = "";
+if (process.env.GITHUB_ACTION) {
+  console.log("GITHUB_ACTION detected");
+  pathBase = "uni-os-helper";
+}
+
 export default {
   preprocess: sveltePreprocess({
     postcss: true,
@@ -7,12 +15,10 @@ export default {
   kit: {
     adapter: adapter(),
 
-    // default options are shown. On some platforms
-    // these options are set automatically — see below
-    // pages: 'build',
-    // assets: 'build',
-    // fallback: null,
-    // precompress: false
+    paths: {
+      base: pathBase,
+    },
+
     prerender: {
       // This can be false if you're using a fallback (i.e. SPA mode)
       default: true,
