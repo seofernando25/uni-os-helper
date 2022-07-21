6f752be8c78c2e4d99d44f46934d4d2b8195ab34
Fernando Nogueira <abert036@uottawa.ca>
2022-07-21 09:52:15 -0400

    wa

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,12 @@
 import sveltePreprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
-// Check if environment variables contains GITHUB_ACTION
-let pathBase = "";
-if (process.env.GITHUB_ACTION) {
-  console.log("GITHUB_ACTION detected");
-  pathBase = "/uni-os-helper";
-}
-
 export default {
   preprocess: sveltePreprocess({
     postcss: true,
   }),
   kit: {
     adapter: adapter(),
-
-    paths: {
-      base: pathBase,
-    },
 
     prerender: {
       // This can be false if you're using a fallback (i.e. SPA mode)
